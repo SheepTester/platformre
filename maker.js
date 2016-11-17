4173b90fe8643f4d08833d3b484f254856de8f93
gamedemos <illusionmentmc@gmail.com>
2016-11-17 09:29:35 -0800

    Update maker.js

--- maker.js
+++ maker.js
@@ -168,7 +168,7 @@ document.querySelector("#load").onclick=function(){
 }
 document.querySelector("#test").onclick=function(){
   save();
-  window.location.href=window.location.href.slice(0,window.location.href.lastIndexOf("/")+1)+"platform.html";
+  window.location.href=window.location.href.slice(0,window.location.href.lastIndexOf("/")+1)+"test.html";
   return false;
 }
 document.querySelector("#template").onclick=function(){
