ee8a1a5c7df27613f555adfd265d249e592fe7fa
Sean <ussa.say@gmail.com>
2017-03-12 20:58:44 -0700

    Oh boy minor changes!

--- nova/render.js
+++ nova/render.js
@@ -1,4 +1,4 @@
-function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
+function Render(canvas,BLOCKHEIGHT,physics) {
   var c,
   t={},
   PIXEL_RATIO=SHEEP.pixelratio(),
@@ -46,20 +46,36 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
       t.destX=Math.floor(scrollX/BLOCKHEIGHT)+t.width/BLOCKHEIGHT+1;
       for (var j=t.c;j<t.destX;j++) {
         if ((t.last&&t.last.t)!==(level[i]&&level[i][j]&&level[i][j].t)) {
-          if (t.last&&t.last.t!=='air') {
-            rect("#"+colours[t.last.t],t.x,t.y,BLOCKHEIGHT,j-t.c);
-            if (typeof images[t.last.t]==='object')
+          if (t.last&&physics[t.last.t].colour) {
+            rect("#"+physics[t.last.t].colour,t.x,t.y,BLOCKHEIGHT,j-t.c);
+            if (typeof physics[t.last.t].imageThing==='object')
               for (var k=0;k<j-t.c;k++)
-                c.drawImage(images[t.last.t],t.x+k*BLOCKHEIGHT+BLOCKHEIGHT/5,t.y+BLOCKHEIGHT/5,BLOCKHEIGHT*0.6,BLOCKHEIGHT*0.6);
+                c.drawImage(
+                  physics[t.last.t].imageThing,
+                  t.x+k*BLOCKHEIGHT+BLOCKHEIGHT*(physics[t.last.t].imageSize!==undefined?0.5-physics[t.last.t].imageSize/2:0.2),
+                  t.y+BLOCKHEIGHT*(physics[t.last.t].imageSize!==undefined?0.5-physics[t.last.t].imageSize/2:0.2),
+                  BLOCKHEIGHT*(physics[t.last.t].imageSize||0.6),
+                  BLOCKHEIGHT*(physics[t.last.t].imageSize||0.6)
+                );
+            if (physics[t.last.t].topOnly)
+              rect('rgba(0,0,0,0.2)',t.x,t.y,BLOCKHEIGHT/8,(j-t.c)*8);
           }
           t.x+=(j-t.c)*BLOCKHEIGHT;
           t.c=j;
           t.last=level[i]&&level[i][j];
         }
       }
-      if (t.last&&t.last.t!=='air') {
-        rect("#"+colours[t.last.t],t.x,t.y,BLOCKHEIGHT,j-t.c);
-        if (typeof images[t.last.t]==='object') c.drawImage(images[t.last.t],t.x,t.y,BLOCKHEIGHT,BLOCKHEIGHT);
+      if (t.last&&physics[t.last.t].colour) {
+        rect("#"+physics[t.last.t].colour,t.x,t.y,BLOCKHEIGHT,j-t.c);
+        if (typeof physics[t.last.t].imageThing==='object')
+          for (var k=0;k<j-t.c;k++)
+            c.drawImage(
+              physics[t.last.t].imageThing,
+              t.x+k*BLOCKHEIGHT+BLOCKHEIGHT*(0.5-physics[t.last.t].imageSize/2||0.2),
+              t.y+BLOCKHEIGHT*(0.5-physics[t.last.t].imageSize/2||0.2),
+              BLOCKHEIGHT*(physics[t.last.t].imageSize||0.6),
+              BLOCKHEIGHT*(physics[t.last.t].imageSize||0.6)
+            );
       }
       t.x+=(j-t.c)*BLOCKHEIGHT-t.width-BLOCKHEIGHT;
       t.y+=BLOCKHEIGHT;
@@ -74,7 +90,7 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
       y=Math.floor(-(this.player.y+dy)/BLOCKHEIGHT);
       return level[y]&&level[y][x]||{t:'air'};
     };
-    o=(input1,input2)=>input1===undefined?input2:input1;
+    o=(input1,input2)=>input1===undefined||isNaN(input1)?input2:input1;
     var keys={
       left:keypresses[37]||keypresses[65],
       right:keypresses[39]||keypresses[68],
@@ -84,7 +100,7 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
     };
     this.player.x+=this.player.xv;
     this.player.y+=this.player.yv;
-    // horizontal movement
+    // horizontal collision
     if (physics[blockAt(-15,0).t].hardness==='solid'&&this.player.xv<0) {
       this.player.x=Math.ceil((this.player.x-15)/40)*40+15;
       if (keys.up) {
@@ -103,7 +119,7 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
       else if (keys.left||keys.right) this.player.xv*=-0.5;
       else this.player.xv=0;
     }
-    // vertical movement
+    // vertical collision
     if ((physics[blockAt(-14,-14).t].hardness==='solid'||physics[blockAt(13,-14).t].hardness==='solid')&&this.player.yv<0) {
       this.player.y=Math.ceil((this.player.y-15)/40)*40+15;
       this.player.yv=0;
@@ -118,17 +134,23 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
       physics[blockAt(-14,-13).t].hardness,
       physics[blockAt(13,-13).t].hardness
     ].includes('liquid')) {
+      var blockPhys={};
+      if (physics[blockAt(0,-15).t].hardness==='solid') blockPhys=physics[blockAt(0,-15).t];
+      else if (physics[blockAt(0,16).t].hardness==='solid') blockPhys=physics[blockAt(0,16).t];
+      else if (physics[blockAt(-16,0).t].hardness==='solid') blockPhys=physics[blockAt(-16,0).t];
+      else if (physics[blockAt(15,0).t].hardness==='solid') blockPhys=physics[blockAt(15,0).t];
+      if (blockPhys.xboost!==undefined) this.player.xv+=blockPhys.xboost;
       if (keys.space) {
-        this.player.xv*=0.8;
-        this.player.yv*=0.8;
+        this.player.xv*=o(blockPhys.breakforce/0.375,0.8);
+        this.player.yv*=o(blockPhys.breakforce/0.375,0.8);
       } else {
-        this.player.xv*=0.95;
-        this.player.yv*=0.95;
+        this.player.xv*=(blockPhys.friction*1.1875,0.95);
+        this.player.yv*=(blockPhys.friction*1.1875,0.95);
       }
-      if (keys.up) this.player.yv+=1;
-      if (keys.left) this.player.xv-=1;
-      if (keys.right) this.player.xv+=1;
-      if (keys.down) this.player.yv-=1;
+      if (keys.up) this.player.yv+=o(blockPhys.acceleration/1.5,1);
+      if (keys.left) this.player.xv-=o(blockPhys.acceleration/1.5,1);
+      if (keys.right) this.player.xv+=o(blockPhys.acceleration/1.5,1);
+      if (keys.down) this.player.yv-=o(blockPhys.acceleration/1.5,1);
     } else if (physics[blockAt(-14,-15).t].hardness==='solid'||physics[blockAt(13,-15).t].hardness==='solid') {
       var blockPhys=physics[blockAt(0,-15).t];
       if (blockPhys.xboost!==undefined) this.player.xv+=blockPhys.xboost;
@@ -146,10 +168,10 @@ function Render(canvas,BLOCKHEIGHT,colours,images,physics) {
 }
 Render.loadImages=images=>{
   for (var span in images) {
-    (span=>{
+    if (images[span].image) (span=>{
       var img=new Image();
-      img.src=images[span];
-      img.onload=e=>images[span]=img;
+      img.src=images[span].image;
+      img.onload=e=>images[span].imageThing=img;
     })(span);
   }
 }
