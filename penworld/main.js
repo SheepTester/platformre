93a28b74e4068a5fc253f80a3703414514841592
Sean <ussa.say@gmail.com>
2017-10-24 20:30:31 -0700

    "nothing"

--- penworld/main.js
+++ penworld/main.js
@@ -156,18 +156,20 @@ function polygon(colour,vertices,clockwise) {
 var chunks={},
 chunkfaces={},
 blockData={ // -X +X -Y +Y -Z +Z (left right top bottom front back)
-  "undefined":{opacity:0},
-  "null":{opacity:0},
-  "air":{opacity:0},
-  "grass":{colours:['#866247','#866247','#1F9D06','#866247','#866247','#866247'],opacity:1},
-  "dirt":{colours:'#866247',opacity:1},
-  "stone":{colours:'#919596',opacity:1},
-  "sand":{colours:'#EED38B',opacity:1},
-  "gravel":{colours:'#B8BCBD',opacity:1},
-  "seawater":{colours:'rgba(105,210,231,0.8)',opacity:0},
-  "water":{colours:'rgba(167,219,216,0.8)',opacity:0},
-  "vapour":{colours:'rgba(255,255,255,0.5)',opacity:0}
-};
+  "undefined":{opaque:0,selectable:0},
+  "null":{opaque:0,selectable:0},
+  "air":{opaque:0,selectable:0},
+  "grass":{colours:['#866247','#866247','#1F9D06','#866247','#866247','#866247'],opaque:1,selectable:1},
+  "dirt":{colours:'#866247',opaque:1,selectable:1},
+  "stone":{colours:'#919596',opaque:1,selectable:1},
+  "sand":{colours:'#EED38B',opaque:1,selectable:1},
+  "gravel":{colours:'#B8BCBD',opaque:1,selectable:1},
+  "seawater":{colours:'rgba(105,210,231,0.8)',opaque:0,selectable:2},
+  "water":{colours:'rgba(167,219,216,0.8)',opaque:0,selectable:2},
+  "vapour":{colours:'rgba(255,255,255,0.5)',opaque:0,selectable:2}
+},
+listOfBlocksWithShowingFacesThatYouShouldCheckOut=[];
+listOfBlocksWithShowingFacesThatYouShouldCheckOut.faces={};
 function mod(a,b) {
   return (a%b+b)%b;
 }
@@ -175,88 +177,155 @@ function generateChunk(chx,chy,chz) {
   var blocks=[];
   for (var i=0;i<config.CHUNK_SIZE*config.CHUNK_SIZE*config.CHUNK_SIZE;i++) blocks.push({type:"grass"});
   chunks[`${chx},${chy},${chz}`]=blocks;
+  for (var y=0;y<config.CHUNK_SIZE;y++) {
+    if (generateChunk.opaqueLayer(chx,chy,chz,y)
+        &&generateChunk.opaqueLayer(chx,chy,chz,y-1)
+        &&generateChunk.opaqueLayer(chx,chy,chz,y+1)
+        &&generateChunk.opaqueXs(chx,chy,chz,z-1,y)
+        &&generateChunk.opaqueXs(chx,chy,chz,z+config.CHUNK_SIZE+1,y)
+        &&generateChunk.opaqueZs(chx,chy,chz,x-1,y)
+        &&generateChunk.opaqueZs(chx,chy,chz,x+config.CHUNK_SIZE+1,y));
+    else {
+      for (var x=0;x<config.CHUNK_SIZE;x++) for (var z=0;z<config.CHUNK_SIZE;z++) {
+        updateBlockFaces(chx*config.CHUNK_SIZE+x,chy*config.CHUNK_SIZE+y,chz*config.CHUNK_SIZE+z);
+      }
+    }
+  }
 }
-function block(x,y,z,newblock) {
+generateChunk.opaqueLayer=(chx,chy,chz,oy)=>{
+  var y=mod(oy,config.CHUNK_SIZE),
+  chunk=chunks[`${chx},${chy+(oy-y)/config.CHUNK_SIZE},${chz}`]||[];
+  for (var x=0;x<config.CHUNK_SIZE;x++) for (var z=0;z<config.CHUNK_SIZE;z++) {
+    if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opaque) return false;
+  }
+  return true;
+}
+generateChunk.opaqueXs=(chx,chy,chz,oz,oy)=>{
+  var z=mod(oz,config.CHUNK_SIZE),
+  y=mod(oy,config.CHUNK_SIZE),
+  chunk=chunks[`${chx},${chy+(oy-y)/config.CHUNK_SIZE},${chz+(oz-z)/config.CHUNK_SIZE}`]||[];
+  for (var x=0;x<config.CHUNK_SIZE;x++) {
+    if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opaque) return false;
+  }
+  return true;
+}
+generateChunk.opaqueZs=(chx,chy,chz,ox,oy)=>{
+  var x=mod(ox,config.CHUNK_SIZE),
+  y=mod(oy,config.CHUNK_SIZE),
+  chunk=chunks[`${chx+(ox-x)/config.CHUNK_SIZE},${chy+(oy-y)/config.CHUNK_SIZE},${chz}`]||[];
+  for (var z=0;z<config.CHUNK_SIZE;z++) {
+    if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opaque) return false;
+  }
+  return true;
+}
+function block(x,y,z,value,prop='type') {
   var chunk=`${Math.floor(x/config.CHUNK_SIZE)},${Math.floor(y/config.CHUNK_SIZE)},${Math.floor(z/config.CHUNK_SIZE)}`;
   if (chunks[chunk]) {
-    if (newblock) return chunks[chunk][(mod(z,config.CHUNK_SIZE)*config.CHUNK_SIZE+mod(y,config.CHUNK_SIZE))*config.CHUNK_SIZE+mod(x,config.CHUNK_SIZE)]=newblock;
+    if (value) {
+      if (prop==='overwrite') return chunks[chunk][(mod(z,config.CHUNK_SIZE)*config.CHUNK_SIZE+mod(y,config.CHUNK_SIZE))*config.CHUNK_SIZE+mod(x,config.CHUNK_SIZE)]=value;
+      else return chunks[chunk][(mod(z,config.CHUNK_SIZE)*config.CHUNK_SIZE+mod(y,config.CHUNK_SIZE))*config.CHUNK_SIZE+mod(x,config.CHUNK_SIZE)][prop]=value;
+    }
     else return chunks[chunk][(mod(z,config.CHUNK_SIZE)*config.CHUNK_SIZE+mod(y,config.CHUNK_SIZE))*config.CHUNK_SIZE+mod(x,config.CHUNK_SIZE)];
   }
   else return {};
 }
-function makeChunkFaces(chx,chy,chz) {
-  var chunk=chunks[`${chx},${chy},${chz}`],
-  faces=[];
-  if (!chunk) return [];
-  function opaqueLayer(oy) {
-    var y=mod(oy,config.CHUNK_SIZE),
-    chunk=chunks[`${chx},${chy+(oy-y)/config.CHUNK_SIZE},${chz}`]||[];
-    for (var x=0;x<config.CHUNK_SIZE;x++) for (var z=0;z<config.CHUNK_SIZE;z++) {
-      if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opacity) return false;
-    }
-    return true;
+function updateBlockFaces(x,y,z) {
+  var bl=block(x,y,z).type,faces=[],
+  colours=blockData[bl].colours;
+  function makeObj(side,pos1,pos2) {
+    return {
+      colour:colours[side],
+      ccw:(side+(side===2||side===3))%2,
+      axis:(side/2)>>0,
+      x:x*config.BLOCK_SIZE+(side===1&&config.BLOCK_SIZE),
+      y:y*config.BLOCK_SIZE+(side===3&&config.BLOCK_SIZE),
+      z:z*config.BLOCK_SIZE+(side===5&&config.BLOCK_SIZE),
+      pos1:pos1*config.BLOCK_SIZE+config.BLOCK_SIZE,
+      pos2:pos2*config.BLOCK_SIZE+config.BLOCK_SIZE
+    };
   }
-  function opaqueXs(oz,oy) {
-    var z=mod(oz,config.CHUNK_SIZE),
-    y=mod(oy,config.CHUNK_SIZE),
-    chunk=chunks[`${chx},${chy+(oy-y)/config.CHUNK_SIZE},${chz+(oz-z)/config.CHUNK_SIZE}`]||[];
-    for (var x=0;x<config.CHUNK_SIZE;x++) {
-      if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opacity) return false;
-    }
-    return true;
+  if (typeof colours==='string') colours=[colours,colours,colours,colours,colours,colours];
+  if (blockData[bl].opaque) {
+    if (!blockData[block(x-1,y,z).type].opaque) faces.push(makeObj(0,y,z));
+    if (!blockData[block(x+1,y,z).type].opaque) faces.push(makeObj(1,y,z));
+    if (!blockData[block(x,y-1,z).type].opaque) faces.push(makeObj(2,x,z));
+    if (!blockData[block(x,y+1,z).type].opaque) faces.push(makeObj(3,x,z));
+    if (!blockData[block(x,y,z-1).type].opaque) faces.push(makeObj(4,x,y));
+    if (!blockData[block(x,y,z+1).type].opaque) faces.push(makeObj(5,x,y));
+  } else if (colours) {
+    if (block(x-1,y,z).type!==bl) faces.push(makeObj(0,y,z));
+    if (block(x+1,y,z).type!==bl) faces.push(makeObj(1,y,z));
+    if (block(x,y-1,z).type!==bl) faces.push(makeObj(2,x,z));
+    if (block(x,y+1,z).type!==bl) faces.push(makeObj(3,x,z));
+    if (block(x,y,z-1).type!==bl) faces.push(makeObj(4,x,y));
+    if (block(x,y,z+1).type!==bl) faces.push(makeObj(5,x,y));
   }
-  function opaqueZs(ox,oy) {
-    var x=mod(ox,config.CHUNK_SIZE),
-    y=mod(oy,config.CHUNK_SIZE),
-    chunk=chunks[`${chx+(ox-x)/config.CHUNK_SIZE},${chy+(oy-y)/config.CHUNK_SIZE},${chz}`]||[];
-    for (var z=0;z<config.CHUNK_SIZE;z++) {
-      if (!blockData[(chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{}).type].opacity) return false;
+  block(x,y,z,faces,'faces');
+  if (faces.length) {
+    if (!~listOfBlocksWithShowingFacesThatYouShouldCheckOut.indexOf(`${x} ${y} ${z}`)) {
+      listOfBlocksWithShowingFacesThatYouShouldCheckOut.push(`${x} ${y} ${z}`);
     }
-    return true;
-  }
-  for (var y=0;y<config.CHUNK_SIZE;y++) {
-    if (opaqueLayer(y)&&opaqueLayer(y-1)&&opaqueLayer(y+1)&&opaqueXs(z-1,y)&&opaqueXs(z+config.CHUNK_SIZE+1,y)&&opaqueZs(x-1,y)&&opaqueZs(x+config.CHUNK_SIZE+1,y)) {
-      //
-    } else {
-      for (var x=0;x<config.CHUNK_SIZE;x++) for (var z=0;z<config.CHUNK_SIZE;z++) {
-        var t=chunk[(z*config.CHUNK_SIZE+y)*config.CHUNK_SIZE+x]||{};
-        if (blockData[t.type].opacity) {
-          var coords=[(chx*config.CHUNK_SIZE+x)*config.BLOCK_SIZE,(chy*config.CHUNK_SIZE+y)*config.BLOCK_SIZE,(chz*config.CHUNK_SIZE+z)*config.BLOCK_SIZE],
-          colours=blockData[t.type].colours;
-          if (typeof colours==='string') colours=[colours,colours,colours,colours,colours,colours];
-          if (!blockData[block(chx*config.CHUNK_SIZE+x-1,chy*config.CHUNK_SIZE+y,chz*config.CHUNK_SIZE+z).type].opacity)
-            faces.push([colours[0],0,0,...coords,coords[1]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          if (!blockData[block(chx*config.CHUNK_SIZE+x+1,chy*config.CHUNK_SIZE+y,chz*config.CHUNK_SIZE+z).type].opacity)
-            faces.push([colours[1],0,1,coords[0]+config.CHUNK_SIZE,coords[1],coords[2],coords[1]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          if (!blockData[block(chx*config.CHUNK_SIZE+x,chy*config.CHUNK_SIZE+y-1,chz*config.CHUNK_SIZE+z).type].opacity)
-            faces.push([colours[2],1,1,...coords,coords[0]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          if (!blockData[block(chx*config.CHUNK_SIZE+x,chy*config.CHUNK_SIZE+y+1,chz*config.CHUNK_SIZE+z).type].opacity)
-            faces.push([colours[3],1,0,coords[0],coords[1]+config.CHUNK_SIZE,coords[2],coords[0]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          if (!blockData[block(chx*config.CHUNK_SIZE+x,chy*config.CHUNK_SIZE+y,chz*config.CHUNK_SIZE+z-1).type].opacity)
-            faces.push([colours[4],2,0,...coords,coords[0]+config.BLOCK_SIZE,coords[1]+config.BLOCK_SIZE]);
-          if (!blockData[block(chx*config.CHUNK_SIZE+x,chy*config.CHUNK_SIZE+y,chz*config.CHUNK_SIZE+z+1).type].opacity)
-            faces.push([colours[5],2,1,coords[0],coords[1],coords[2]+config.CHUNK_SIZE,coords[0]+config.BLOCK_SIZE,coords[1]+config.BLOCK_SIZE]);
-        } else if (blockData[t.type].colours) {
-          var coords=[(chx*config.CHUNK_SIZE+x)*config.BLOCK_SIZE,(chy*config.CHUNK_SIZE+y)*config.BLOCK_SIZE,(chz*config.CHUNK_SIZE+z)*config.BLOCK_SIZE],
-          colours=blockData[t.type].colours;
-          if (typeof colours==='string') colours=[colours,colours,colours,colours,colours,colours];
-          faces.push([colours[0],0,0,...coords,coords[1]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          faces.push([colours[1],0,1,coords[0]+config.CHUNK_SIZE,coords[1],coords[2],coords[1]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          faces.push([colours[2],1,1,...coords,coords[0]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          faces.push([colours[3],1,0,coords[0],coords[1]+config.CHUNK_SIZE,coords[2],coords[0]+config.BLOCK_SIZE,coords[2]+config.BLOCK_SIZE]);
-          faces.push([colours[4],2,0,...coords,coords[0]+config.BLOCK_SIZE,coords[1]+config.BLOCK_SIZE]);
-          faces.push([colours[5],2,1,coords[0],coords[1],coords[2]+config.CHUNK_SIZE,coords[0]+config.BLOCK_SIZE,coords[1]+config.BLOCK_SIZE]);
-        }
-      }
+    listOfBlocksWithShowingFacesThatYouShouldCheckOut.faces[`${x} ${y} ${z}`]=faces;
+  } else {
+    var t;
+    if (~(t=listOfBlocksWithShowingFacesThatYouShouldCheckOut.indexOf(`${x} ${y} ${z}`))) {
+      listOfBlocksWithShowingFacesThatYouShouldCheckOut.splice(t,1);
     }
   }
   return faces;
 }
+function sign(n) {
+  return n>0?1:n<0?-1:0;
+}
+function rayCollides(origin,towards,isSolid,limit=7) {
+  // origin, towards - points; they are expected to be <=1un from each other
+  // need to scale coord system to 1 cube un per block
+  var xdist=Math.abs(towards.x-origin.x),
+  ydist=Math.abs(towards.y-origin.y)
+  zdist=Math.abs(towards.z-origin.z),
+  signx=sign(towards.x-origin.x),
+  signy=sign(towards.y-origin.y),
+  signz=sign(towards.z-origin.z),
+  offsetx=signx>0?mod(origin.x,1):mod(-origin.x,1),
+  offsety=signy>0?mod(origin.y,1):mod(-origin.y,1),
+  offsetz=signz>0?mod(origin.z,1):mod(-origin.z,1),
+  lastx=1,
+  lasty=1,
+  lastz=1,
+  coords=[Math.floor(origin.x),Math.floor(origin.y),Math.floor(origin.z)];
+  if (isSolid(...coords)) return pt(...coords);
+  for (var i=0;i<limit;i++) {
+    while (i*xdist+offsetx>lastx) {
+      coords=[Math.floor(lastx*signx+origin.x),Math.floor((lastx-offsetx)/xdist*ydist*signy+origin.y),Math.floor((lastx-offsetx)/xdist*zdist*signz+origin.z)];
+      if (isSolid(...coords)) return pt(...coords);
+      lastx++;
+    }
+    while (i*ydist+offsety>lasty) {
+      coords=[Math.floor((lasty-offsety)/ydist*xdist*signx+origin.x),Math.floor(lasty*signy+origin.y),Math.floor((lasty-offsety)/ydist*zdist*signz+origin.z)];
+      if (isSolid(...coords)) return pt(...coords);
+      lasty++;
+    }
+    while (i*zdist+offsetz>lastz) {
+      coords=[Math.floor((lastz-offsetz)/zdist*ydist*signx+origin.x),Math.floor((lastz-offsetz)/zdist*ydist*signy+origin.y),Math.floor(lastz*signz+origin.z)];
+      if (isSolid(...coords)) return pt(...coords);
+      lastz++;
+    }
+  }
+  return null;
+}
 generateChunk(0,0,0);
-for (var i=2,blocks=Object.keys(blockData);i<blocks.length;i++) block(i-2,0,0,{type:blocks[i]});
+for (var i=2,blocks=Object.keys(blockData);i<blocks.length;i++) {
+  block(i-2,0,0,blocks[i]);
+  updateBlockFaces(i-2,0,0);
+  updateBlockFaces(i-3,0,0);
+  updateBlockFaces(i-1,0,0);
+  updateBlockFaces(i-2,-1,0);
+  updateBlockFaces(i-2,1,0);
+  updateBlockFaces(i-2,0,-1);
+  updateBlockFaces(i-2,0,1);
+}
 
 /* MAKES THINGS HAPPEN */
-var shapes=makeChunkFaces(0,0,0);
 function draw() {
   c.clearRect(-innerWidth/2,-innerHeight/2,innerWidth,innerHeight);
   camera.x+=camera.xv,camera.xv*=0.9;
@@ -272,6 +341,16 @@ function draw() {
   if (keys[68]) camera.xv+=camera.rotx.cos*config.MOVE_SPEED,camera.zv-=camera.rotx.sin*config.MOVE_SPEED;
   if (keys[16]) camera.yv+=config.MOVE_SPEED;
   if (keys[32]) camera.yv-=config.MOVE_SPEED;
+  var startpt=pt(camera.x/config.BLOCK_SIZE,camera.y/config.BLOCK_SIZE,camera.z/config.BLOCK_SIZE); // /config.BLOCK_SIZE to scale to 1un coordinate system thing
+  var z=(camera.roty.cos*config.BLOCK_SIZE)/(camera.roty.sin*camera.roty.sin+camera.roty.cos*camera.roty.cos),
+  y=(camera.roty.sin*z)/camera.roty.cos,
+  x;
+  z=(camera.rotx.cos*z)/(camera.rotx.sin*camera.rotx.sin+camera.rotx.cos*camera.rotx.cos),
+  x=(camera.rotx.sin*z)/camera.rotx.cos;
+  var raypt=pt((x+camera.x)/config.BLOCK_SIZE,(y+camera.y)/config.BLOCK_SIZE,(z+camera.z)/config.BLOCK_SIZE);
+  var selected=rayCollides(startpt,raypt,(x,y,z)=>{
+    return blockData[block(x,y,z).type].selectable;
+  },7);
   /*c.fillStyle='#866247';
   polygon([pt(-50,-50,50),pt(50,-50,50),pt(50,50,50),pt(-50,50,50)],'ccw');
   polygon([pt(-50,-50,-50),pt(50,-50,-50),pt(50,50,-50),pt(-50,50,-50)],'cw');
@@ -284,32 +363,36 @@ function draw() {
   polygon([pt(-50,-50,-50),pt(50,-50,-50),pt(50,-40,-50),pt(-50,-40,-50)],'cw');
   polygon([pt(50,-50,-50),pt(50,-50,50),pt(50,-40,50),pt(50,-40,-50)],'cw');
   polygon([pt(-50,-50,-50),pt(-50,-50,50),pt(-50,-40,50),pt(-50,-40,-50)],'ccw');*/
-  var faces=[];
+  var faces=[],
+  shapes=[];
+  for (var i=0;i<listOfBlocksWithShowingFacesThatYouShouldCheckOut.length;i++)
+    shapes.push(...listOfBlocksWithShowingFacesThatYouShouldCheckOut.faces[listOfBlocksWithShowingFacesThatYouShouldCheckOut[i]]);
+  if (keys[13]) console.log(shapes);
   for (var i=0;i<shapes.length;i++) {
-    switch (shapes[i][1]) {
+    switch (shapes[i].axis) {
       case 0: // X stays the same
-        faces.push(polygon(shapes[i][0],[
-          pt(shapes[i][3],shapes[i][4],shapes[i][5]),
-          pt(shapes[i][3],shapes[i][6],shapes[i][5]),
-          pt(shapes[i][3],shapes[i][6],shapes[i][7]),
-          pt(shapes[i][3],shapes[i][4],shapes[i][7])
-        ],shapes[i][2]?'ccw':'cw'));
+        faces.push(polygon(shapes[i].colour,[
+          pt(shapes[i].x,shapes[i].y,shapes[i].z),
+          pt(shapes[i].x,shapes[i].pos1,shapes[i].z),
+          pt(shapes[i].x,shapes[i].pos1,shapes[i].pos2),
+          pt(shapes[i].x,shapes[i].y,shapes[i].pos2)
+        ],shapes[i].ccw?'ccw':'cw'));
         break;
       case 1: // Y stays the same
-        faces.push(polygon(shapes[i][0],[
-          pt(shapes[i][3],shapes[i][4],shapes[i][5]),
-          pt(shapes[i][6],shapes[i][4],shapes[i][5]),
-          pt(shapes[i][6],shapes[i][4],shapes[i][7]),
-          pt(shapes[i][3],shapes[i][4],shapes[i][7])
-        ],shapes[i][2]?'ccw':'cw'));
+        faces.push(polygon(shapes[i].colour,[
+          pt(shapes[i].x,shapes[i].y,shapes[i].z),
+          pt(shapes[i].pos1,shapes[i].y,shapes[i].z),
+          pt(shapes[i].pos1,shapes[i].y,shapes[i].pos2),
+          pt(shapes[i].x,shapes[i].y,shapes[i].pos2)
+        ],shapes[i].ccw?'ccw':'cw'));
         break;
       case 2: // Z stays the same
-        faces.push(polygon(shapes[i][0],[
-          pt(shapes[i][3],shapes[i][4],shapes[i][5]),
-          pt(shapes[i][6],shapes[i][4],shapes[i][5]),
-          pt(shapes[i][6],shapes[i][7],shapes[i][5]),
-          pt(shapes[i][3],shapes[i][7],shapes[i][5])
-        ],shapes[i][2]?'ccw':'cw'));
+        faces.push(polygon(shapes[i].colour,[
+          pt(shapes[i].x,shapes[i].y,shapes[i].z),
+          pt(shapes[i].pos1,shapes[i].y,shapes[i].z),
+          pt(shapes[i].pos1,shapes[i].pos2,shapes[i].z),
+          pt(shapes[i].x,shapes[i].pos2,shapes[i].z)
+        ],shapes[i].ccw?'ccw':'cw'));
         break;
     }
   }
