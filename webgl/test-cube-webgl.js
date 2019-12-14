8e1ad614007e88a5be7fe4d49a6c4b861ea28163
SheepTester <ussa.say@gmail.com>
2019-12-14 15:42:54 -0800

    :kaaba:

--- webgl/test-cube-webgl.js
+++ webgl/test-cube-webgl.js
@@ -72,10 +72,41 @@ function initBuffers (gl) {
   const positionBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
   const positions = [
-    -1.0,  1.0,
-     1.0,  1.0,
-    -1.0, -1.0,
-     1.0, -1.0,
+    // Front face
+    -1.0, -1.0,  1.0,
+     1.0, -1.0,  1.0,
+     1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+     1.0,  1.0,  1.0,
+     1.0,  1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+     1.0, -1.0, -1.0,
+     1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+
+    // Right face
+     1.0, -1.0, -1.0,
+     1.0,  1.0, -1.0,
+     1.0,  1.0,  1.0,
+     1.0, -1.0,  1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0,
   ]
   gl.bufferData(
     gl.ARRAY_BUFFER,
@@ -83,20 +114,44 @@ function initBuffers (gl) {
     gl.STATIC_DRAW
   )
 
-  const colours = [
-    81, 91, 212, 255, // blue
-    129, 52, 175, 255, // purple
-    254, 218, 119, 255, // yellow
-    221, 42, 123, 255 // magenta
-  ].map(c => c / 255)
+  const faceColours = [
+    [233, 30, 99], // pink
+    [156, 39, 176], // purple
+    [63, 81, 181], // indigo
+    [33, 150, 243], // blue
+    [0, 188, 212], // cyan
+    [0, 150, 136] // teal
+  ].map(c => [...c.map(ch => ch / 255), 1])
+  // Convert array of colours into table for each vertex
+  const colours = []
+  for (const colour of faceColours) {
+    // Four times for each vertex of face
+    colours.push(...colour, ...colour, ...colour, ...colour)
+  }
 
   const colourBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, colourBuffer)
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colours), gl.STATIC_DRAW)
 
+  const indexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  // Following array defines each face as two triangles referencing indices of
+  // vertex array to specify triangles' positions (an "element array")
+  const indices = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23,   // left
+  ]
+  // Give array to WebGL
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
+
   return {
     position: positionBuffer,
-    colour: colourBuffer
+    colour: colourBuffer,
+    indices: indexBuffer
   }
 }
 const buffers = initBuffers(gl)
@@ -121,11 +176,12 @@ function drawScene (gl, programInfo, buffers, elapsedTime) {
   const modelViewMatrix = mat4.create()
   mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0])
   mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [0, 1, 1])
+  // mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation * 0.7, [0, 1, 0])
 
   gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position)
   gl.vertexAttribPointer(
     programInfo.attrLocs.vertexPosition,
-    2,
+    3,
     gl.FLOAT,
     false,
     0,
@@ -144,6 +200,9 @@ function drawScene (gl, programInfo, buffers, elapsedTime) {
   )
   gl.enableVertexAttribArray(programInfo.attrLocs.vertexColour)
 
+  // Tell which indices to use to index the vertices
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices)
+
   gl.useProgram(programInfo.program)
 
   gl.uniformMatrix4fv(
@@ -157,9 +216,10 @@ function drawScene (gl, programInfo, buffers, elapsedTime) {
     modelViewMatrix
   )
 
+  const vertexCount = 36
+  const type = gl.UNSIGNED_SHORT
   const offset = 0
-  const vertexCount = 4
-  gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount)
+  gl.drawElements(gl.TRIANGLES, vertexCount, type, offset)
 }
 
 let lastTime = Date.now()
