50d73e23d5f1fef3aee6a5fc0e2b025532192d44
SheepTester <ussa.say@gmail.com>
2019-12-15 13:52:26 -0800

    Well, at least *something* appeared

--- webgl/penworld.js
+++ webgl/penworld.js
@@ -42,7 +42,7 @@ const textureAtlasPromise = Promise.all(textureURLs.map(url =>
   new Promise(resolve => {
     const image = new Image()
     image.onload = e => {
-      resolve(image)
+      resolve({ url, image })
     }
     image.src = url
   })))
@@ -54,43 +54,143 @@ const textureAtlasPromise = Promise.all(textureURLs.map(url =>
     canvas.width = columns * textureSize
     canvas.height = rows * textureSize
     for (let i = 0; i < images.length; i++) {
-      c.drawImage(images[i], i % columns * textureSize, (i / columns | 0) * textureSize)
+      const { url, image } = images[i]
+      const col = i % columns
+      const row = i / columns | 0
+      textures[url] = {
+        image,
+        coords: [
+          col / columns, row / rows,
+          (col + 1) / columns, row / rows,
+          (col + 1) / columns, (row + 1) / rows,
+          col / columns, (row + 1) / rows,
+        ]
+      }
+      c.drawImage(image, col * textureSize, row * textureSize)
     }
     return canvas
   })
 
+const neighbours = [
+  [0, 1, 0, 'top', 'bottom'],
+  [0, -1, 0, 'bottom', 'top'],
+  [1, 0, 0, 'right', 'left'],
+  [-1, 0, 0, 'left', 'right'],
+  [0, 0, 1, 'front', 'back'],
+  [0, 0, -1, 'back', 'front']
+]
+
+let facesChanged = false
+
 class Block {
   constructor (type) {
     this.type = type
+    this.faces = {}
   }
 
   characteristics () {
     return blocks[this.type] || {}
   }
 
+  texture (face) {
+    const { texture } = this.characteristics()
+    return texture ? texture[face] || texture : null
+  }
+
   isSolid () {
     return this.characteristics().solid
   }
 
+  showFace (face) {
+    if (this.faces[face]) return
+    const texture = this.texture(face)
+    const x = this.chunk.x + this.x
+    if (texture) {
+      const { x, y, z } = this
+      const globalX = this.chunk.x * CHUNK_SIZE + x
+      const globalY = this.chunk.y * CHUNK_SIZE + x
+      const globalZ = this.chunk.z * CHUNK_SIZE + x
+      let plane
+      if (face === 'top' || face === 'bottom') {
+        const planeY = face === 'top' ? globalY + 1 : globalY
+        plane = [
+          globalX, planeY, globalZ,
+          globalX + 1, planeY, globalZ,
+          globalX + 1, planeY, globalZ + 1,
+          globalX, planeY, globalZ + 1
+        ]
+      } else if (face === 'left' || face === 'right') {
+        const planeX = face === 'right' ? globalX + 1 : globalX
+        plane = [
+          planeX, globalY, globalZ,
+          planeX, globalY + 1, globalZ,
+          planeX, globalY + 1, globalZ + 1,
+          planeX, globalY, globalZ + 1
+        ]
+      } else if (face === 'front' || face === 'back') {
+        const planeZ = face === 'front' ? globalZ + 1 : globalZ
+        plane = [
+          globalX, globalY, planeZ,
+          globalX + 1, globalY, planeZ,
+          globalX + 1, globalY + 1, planeZ,
+          globalX, globalY + 1, planeZ
+        ]
+      } else {
+        return
+      }
+      this.faces[face] = {
+        plane,
+        coords: textures[texture].coords
+      }
+      if (!facesChanged) facesChanged = true
+    }
+  }
+
+  hideFace (face) {
+    if (this.faces[face]) {
+      delete this.faces[face]
+      if (!facesChanged) facesChanged = true
+    }
+  }
+
+  neighbourIsSolid (offsetX = 0, offsetY = 0, offsetZ = 0) {
+    const { x, y, z } = this
+    const neighbour = this.chunk.getBlock(x + offsetX, y + offsetY, z + offsetZ)
+    return neighbour ? neighbour.isSolid() : false
+  }
+
   updateFaces () {
-    // TODO: Should update a master Float32Array to show/hide faces for maximum efficiency!
     const amSolid = this.isSolid()
+    const { x, y, z } = this
+    for (const [offsetX, offsetY, offsetZ, myFace, theirFace] of neighbours) {
+      const neighbour = this.chunk.getBlock(x + offsetX, y + offsetY, z + offsetZ)
+      const theyreSolid = neighbour && neighbour.isSolid()
+      if (amSolid && theyreSolid) {
+        this.hideFace(myFace)
+        neighbour.hideFace(theirFace)
+      } else {
+        this.showFace(myFace)
+        if (neighbour) {
+          neighbour.showFace(theirFace)
+        }
+      }
+    }
   }
 }
 
 const CHUNK_SIZE = 16
-const subchunks = {}
 
 class Subchunk {
   constructor (x, y, z) {
-    if (subchunks[`${x},${y},${z}`]) throw new Error('Chunk exists where I am!')
-    subchunks[`${x},${y},${z}`] = this
+    if (Subchunk.subchunks[`${x},${y},${z}`]) {
+      throw new Error('Chunk exists where I am!')
+    }
+    Subchunk.subchunks[`${x},${y},${z}`] = this
 
     this.x = x
     this.y = y
     this.z = z
     this.blocks = new Array(CHUNK_SIZE ** 3).fill(null)
-    this.visibleFaces = []
   }
 
   getChunk (offsetX = 0, offsetY = 0, offsetZ = 0) {
@@ -98,28 +198,91 @@ class Subchunk {
     return subchunks[`${x + offsetX},${y + offsetY},${z + offsetZ}`]
   }
 
+  inChunk (x, y, z) {
+    return x >= 0 && x < CHUNK_SIZE && y >= 0 && y < CHUNK_SIZE && z >= 0 && z < CHUNK_SIZE
+  }
+
   getBlock (x, y, z) {
-    return this.blocks[(y * CHUNK_SIZE + x) * CHUNK_SIZE + z]
+    if (this.inChunk(x, y, z)) {
+      return this.blocks[(y * CHUNK_SIZE + x) * CHUNK_SIZE + z]
+    } else {
+      return Subchunk.getGlobalBlock(
+        this.x * CHUNK_SIZE + x,
+        this.y * CHUNK_SIZE + y,
+        this.z * CHUNK_SIZE + z
+      )
+    }
   }
 
   setBlock (x, y, z, block) {
+    if (!this.inChunk(x, y, z)) {
+      throw new Error('not my block!')
+    }
     if (this.getBlock(x, y, z)) {
       this.blocks[(y * CHUNK_SIZE + x) * CHUNK_SIZE + z].chunk = null
     }
     this.blocks[(y * CHUNK_SIZE + x) * CHUNK_SIZE + z] = block
-    block.chunk = this
-    block.x = x
-    block.y = y
-    block.z = z
-    block.updateFaces()
+    if (block) {
+      block.chunk = this
+      block.x = x
+      block.y = y
+      block.z = z
+      block.updateFaces()
+    }
+  }
+
+  storeFacesIn (faces, textureCoords) {
+    for (const block of this.blocks) {
+      if (block) {
+        for (const { plane, coords } of Object.values(block.faces)) {
+          faces.push(...plane)
+          textureCoords.push(...coords)
+        }
+      }
+    }
+  }
+
+  static getGlobalBlock (x, y, z) {
+    const subchunk = this.subchunks[`${Math.floor(x / CHUNK_SIZE)},${Math.floor(y / CHUNK_SIZE)},${Math.floor(z / CHUNK_SIZE | 0)}`]
+    if (subchunk) {
+      return subchunk.getBlock(mod(x, CHUNK_SIZE), mod(y, CHUNK_SIZE), mod(z, CHUNK_SIZE))
+    } else {
+      return null
+    }
   }
 }
 
+Subchunk.subchunks = {}
+
+const program = makeProgram()
+let buffers
+let textureAtlas
+
 const subchunk = new Subchunk(0, 0, 0)
-for (let y = 0; y < 8; y++) {
-  for (let x = 0; x < CHUNK_SIZE; x++) {
-    for (let z = 0; z < CHUNK_SIZE; z++) {
-      subchunk.setBlock(x, y, z, new Block(y < 4 ? 'stone' : y < 6 ? 'dirt' : 'grass'))
+
+textureAtlasPromise.then(createTexture)
+  .then(texture => {
+    textureAtlas = texture
+
+    // TEMP "world gen"
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < CHUNK_SIZE; x++) {
+        for (let z = 0; z < CHUNK_SIZE; z++) {
+          subchunk.setBlock(x, y, z, new Block(y < 4 ? 'stone' : y < 6 ? 'dirt' : 'grass'))
+        }
+      }
     }
+
+    render()
+  })
+
+function render () {
+  if (facesChanged || !buffers) {
+    const faces = []
+    const textureCoords = []
+    // TEMP; should do all subchunks nearby
+    subchunk.storeFacesIn(faces, textureCoords)
+    buffers = makeBuffers(faces, textureCoords)
   }
+  drawScene(program, buffers, textureAtlas)
 }
--- webgl/renderer.js
+++ webgl/renderer.js
@@ -1,9 +1,172 @@
+const { mat4 } = glMatrix
+
+function mod (a, b) {
+  return (a % b + b) % b
+}
+
 const canvas = document.getElementById('canvas')
 const gl = canvas.getContext('webgl')
 
-textureAtlasPromise.then(canvas => {
+let width, height
+function resize() {
+  // TODO: device pixel ratio
+  width = window.innerWidth
+  height = window.innerHeight
+  canvas.width = width
+  canvas.height = height
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+}
+window.addEventListener('resize', resize)
+resize()
+
+function createTexture (canvas) {
   const texture = gl.createTexture()
   gl.bindTexture(gl.TEXTURE_2D, texture)
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas)
+  gl.generateMipmap(gl.TEXTURE_2D)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
   return texture
-})
+}
+
+function loadShader (gl, type, source) {
+  const shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    const log = gl.getShaderInfoLog(shader)
+    gl.deleteShader(shader)
+    throw new Error('shader compile wucky' + log)
+  }
+  return shader
+}
+function shaderProgram (gl, vsSource, fsSource, attrs, uniforms) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
+
+  const shaderProgram = gl.createProgram()
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    throw new Error('shader program wucky' + gl.getProgramInfoLog(shaderProgram))
+  }
+
+  const attrLocs = {}
+  for (const [prop, attr] of attrs) {
+    attrLocs[prop] = gl.getAttribLocation(shaderProgram, attr)
+  }
+  const uniformLocs = {}
+  for (const [prop, uniform] of uniforms) {
+    uniformLocs[prop] = gl.getUniformLocation(shaderProgram, uniform)
+  }
+  return {
+    program: shaderProgram,
+    attrs: attrLocs,
+    uniforms: uniformLocs
+  }
+}
+function makeProgram () {
+  return shaderProgram(
+    gl,
+    `
+      attribute vec4 aVertexPosition;
+      attribute vec2 aTextureCoord;
+
+      uniform mat4 uModelViewMatrix;
+      uniform mat4 uProjectionMatrix;
+
+      varying highp vec2 vTextureCoord;
+
+      void main() {
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        vTextureCoord = aTextureCoord;
+      }
+    `,
+    `
+      varying highp vec2 vTextureCoord;
+
+      uniform sampler2D uSampler;
+
+      void main() {
+        // Fetches "texel" (texture pixel) from texture; interpolates between
+        // given texture coordinates like colours do
+        gl_FragColor = texture2D(uSampler, vTextureCoord);
+      }
+    `,
+    [
+      ['vertexPosition', 'aVertexPosition'],
+      ['textureCoord', 'aTextureCoord']
+    ],
+    [
+      ['projectionMatrix', 'uProjectionMatrix'],
+      ['modelViewMatrix', 'uModelViewMatrix'],
+      ['uSampler', 'uSampler']
+    ]
+  )
+}
+
+function makeBuffers (faces, textureCoords) {
+  const positionBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(faces), gl.STATIC_DRAW)
+
+  const textureCoordBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW)
+
+  const indexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  const indices = []
+  const indexSets = faces.length / 12
+  for (let i = 0; i < indexSets; i++) {
+    indices.push(i * 6, i * 6 + 1, i * 6 + 2)
+    indices.push(i * 6, i * 6 + 3, i * 6 + 2)
+  }
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
+
+  return {
+    position: positionBuffer,
+    textureCoord: textureCoordBuffer,
+    indices: indexBuffer,
+    vertexCount: indices.length
+  }
+}
+
+function drawScene (programInfo, buffers, texture) {
+  gl.clearColor(54 / 255, 0 / 255, 33 / 255, 1)
+  gl.clearDepth(1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  const projectionMatrix = mat4.create()
+  mat4.perspective(projectionMatrix, 45 * Math.PI / 180, width / height, 0.1, 100)
+
+  const cameraMatrix = mat4.create()
+  mat4.translate(cameraMatrix, cameraMatrix, [0, 0.0, -6.0])
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position)
+  gl.vertexAttribPointer( programInfo.attrs.vertexPosition, 3, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(programInfo.attrs.vertexPosition)
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord)
+  gl.vertexAttribPointer(programInfo.attrs.textureCoord, 2, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(programInfo.attrs.textureCoord)
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices)
+
+  gl.useProgram(programInfo.program)
+
+  gl.uniformMatrix4fv(programInfo.uniforms.projectionMatrix, false, projectionMatrix)
+  gl.uniformMatrix4fv(programInfo.uniforms.modelViewMatrix, false, cameraMatrix)
+
+  gl.activeTexture(gl.TEXTURE0)
+  gl.bindTexture(gl.TEXTURE_2D, texture)
+  gl.uniform1i(programInfo.uniforms.uSampler, 0)
+
+  gl.drawElements(gl.TRIANGLES, buffers.vertexCount, gl.UNSIGNED_SHORT, 0)
+}
